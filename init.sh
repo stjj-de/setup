828e2842a8f79dbb5164f84223fcba29afa4d035
Moritz Ruth <dev@moritzruth.de>
2020-10-12 16:51:17 +0200

    Add docker installation

--- init.sh
+++ init.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+# Docker
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+./update_docker.sh
+
+# Nginx and Certbot
 sudo apt update && sudo apt install -y nginx certbot
 sudo cp -r ./nginx /etc
 
@@ -9,7 +33,5 @@ sudo chown www-data /var/www/_letsencrypt
 
 ./generate_certs.sh
 
-echo -e '#!/bin/bash\nnginx -t && systemctl reload nginx' | tee /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
+echo -e '#!/bin/bash\nnginx -t && systemctl reload nginx' | sudo tee /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
 sudo chmod a+x /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
-
-./update_docker.sh
