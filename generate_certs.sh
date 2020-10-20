aeb087c003547741f97fbb22b5c671ac6ba93d45
Moritz Ruth <dev@moritzruth.de>
2020-10-20 23:39:34 +0200

    Fix generate_certs.sh

--- generate_certs.sh
+++ generate_certs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Comment out TLS related lines
-sudo sed -i -r 's/(listen .*443)/\1;#/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g' /etc/nginx/sites-available/new.stjj.de.conf /etc/nginx/sites-available/analytics.stjj.de.conf
+sudo sed -i -r 's/(listen .*443)/\1;#/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g' /etc/nginx/sites-available/stjj.de.conf /etc/nginx/sites-available/analytics.stjj.de.conf
 
 # Reload
 sudo nginx -t && sudo service nginx reload
@@ -11,7 +11,7 @@ sudo certbot certonly --webroot -d stjj.de -d www.stjj.de --email webmaster@mori
 sudo certbot certonly --webroot -d analytics.stjj.de --email webmaster@moritzruth.de -w /var/www/_letsencrypt -n --agree-tos --force-renewal
 
 # Uncomment TLS related lines
-sudo sed -i -r 's/#?;#//g' /etc/nginx/sites-available/new.stjj.de.conf /etc/nginx/sites-available/analytics.stjj.de.conf
+sudo sed -i -r 's/#?;#//g' /etc/nginx/sites-available/stjj.de.conf /etc/nginx/sites-available/analytics.stjj.de.conf
 
 # Reload again
 sudo nginx -t && sudo service nginx reload
